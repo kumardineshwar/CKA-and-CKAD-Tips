4a0cd1263677ab6cdb82eae073f1d01993ce50b2
kumardineshwar <kumardineshwar@gmail.com>
2020-06-26 15:09:01 +0200

    Update 01-docker-install.sh

--- Installation-Scripts/01-docker-install.sh
+++ Installation-Scripts/01-docker-install.sh
@@ -3,22 +3,23 @@
 # (Install Docker CE)
 ## Set up the repository:
 ### Install packages to allow apt to use a repository over HTTPS
-apt-get update && apt-get install -y \
+sudo apt-get update && apt-get install -y \
   apt-transport-https ca-certificates curl software-properties-common gnupg2
 # Add Docker’s official GPG key:
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 # Add the Docker apt repository:
-add-apt-repository \
+sudo add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) \
   stable"
 # Install Docker CE
-apt-get update && apt-get install -y \
+sudo apt-get update && sudo apt-get install -y \
   containerd.io=1.2.13-1 \
   docker-ce=5:19.03.8~3-0~ubuntu-$(lsb_release -cs) \
   docker-ce-cli=5:19.03.8~3-0~ubuntu-$(lsb_release -cs)
 # Set up the Docker daemon
-cat > /etc/docker/daemon.json <<EOF
+sudo touch  /etc/docker/daemon.json
+sudo cat > /etc/docker/daemon.json <<EOF
 {
   "exec-opts": ["native.cgroupdriver=systemd"],
   "log-driver": "json-file",
@@ -28,9 +29,8 @@ cat > /etc/docker/daemon.json <<EOF
   "storage-driver": "overlay2"
 }
 EOF
-mkdir -p /etc/systemd/system/docker.service.d
+sudo mkdir -p /etc/systemd/system/docker.service.d
 # Restart Docker
-systemctl daemon-reload
-systemctl restart docker
-
+sudo systemctl daemon-reload
+sudo systemctl restart docker
 
