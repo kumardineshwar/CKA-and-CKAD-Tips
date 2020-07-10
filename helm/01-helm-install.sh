093b7a127d2d80e346f21323049cf8cb7a0e17ea
DeeKay <kumardineshwar@gmail.com>
2020-07-10 22:13:06 +0000

    updated helm installation script for ubuntu

---
+++ helm/01-helm-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+curl https://helm.baltorepo.com/organization/signing.asc | sudo apt-key add -
+sudo apt-get install apt-transport-https --yes
+echo "deb https://baltocdn.com/helm/stable/debian/ all main" | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+sudo apt-get update
+sudo apt-get install helm -y
+echo "helm successfully Installed"
+helm version
+
+
