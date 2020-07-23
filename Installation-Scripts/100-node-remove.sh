6e5a4c59880fc646a7475cad0a33dcb2883d73b4
kumardineshwar <kumardineshwar@gmail.com>
2020-07-23 13:00:19 +0000

    update the node remove script

--- Installation-Scripts/100-node-remove.sh
+++ Installation-Scripts/100-node-remove.sh
@@ -3,9 +3,21 @@
 # Use this script to remove nodes including master and worker nodes
 
 
+echo -e "\t\tNode List"
+echo "======================= Node List ============================="
+kubectl get nodes
+
+echo "==============================================================="
+if [ "$1" != "" ]
+then
+	node="$1"
+else
 echo -n "Enter Node name to remove :"
 read node
-
+fi
+echo "removing node $node"
+echo -n "Press Enter to Continue"
+read a
 # disabling nodes 
 kubectl cordon $node
 
