617eedd597d0bd6214487cc832626e6abf14bb2c
kumardineshwar <kumardineshwar@gmail.com>
2021-05-30 17:19:23 +0000

    fix the kubernetest version to install

--- Installation-Scripts/02-kubeadm-install.sh
+++ Installation-Scripts/02-kubeadm-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+VER="1.20.4-00"
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
@@ -8,6 +8,29 @@ EOF
 sudo sysctl --system
 
 sudo modprobe br_netfilter
+if [ "$VER" == "FIXME" ]
+then
+   sudo apt-cache madison kubeadm |head -n 20 | tr -d " "| awk -F "|" '{print $2}' |tee /tmp/k8s-version-to-install
+   read -p "Enter the kubernetes version to install the default will $VER  : " K8S
+   K8S=${K8S:-"1.20.7-00"}
+   if [ "$VER" != "$K8S" ]
+     then
+     sed -i "s/^VER=.*/VER=\"$K8S\"/g" ./02-kubeadm-install.sh
+     VERI=$K8S
+  else
+     VERI=$VER
+  fi
+
+fi
+if [ $(grep -c "$VERI"  /tmp/k8s-version-to-install) -ge 1 ]
+then
+	echo -e "Going to install : $VERI \n Press Enter to continue"
+	read confirm
+else
+	echo "Seems you have not choosen the right kubernetes version : $VERI"
+	exit 1
+fi
+echo $VERI
 
 sudo apt-get update && sudo apt-get install -y apt-transport-https curl
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
@@ -16,10 +39,11 @@ deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
 /usr/sbin/swapoff -a
 sudo apt-get update
-sudo apt-cache madison kubeadm
-sudo apt-get install -y kubelet=1.19.4-00 kubeadm=1.19.4-00 kubectl=1.19.4-00
+#sudo apt-cache madison kubeadm
+sudo apt-get install -y kubelet=$VERI kubeadm=$VERI kubectl=$VERI
 sudo apt-mark hold kubelet kubeadm kubectl
 echo "alias ks=kubectl" >> ~/.bashrc
-#source ~/.bashrc
+source ~/.bashrc
 #installing nfs client on all nodes
 sudo apt install nfs-common -y
+rm -f  /tmp/k8s-version-to-install 2>/dev/null
