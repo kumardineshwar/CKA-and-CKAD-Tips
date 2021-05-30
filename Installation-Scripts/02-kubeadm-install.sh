8a8c8da6e3b5cf4644757006a478d1560dbe9e4e
kumardineshwar <kumardineshwar@gmail.com>
2021-05-30 20:39:28 +0000

    Bug Fix

--- Installation-Scripts/02-kubeadm-install.sh
+++ Installation-Scripts/02-kubeadm-install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-VER="FIXME"
+VER="1.20.7-00"
 cat <<EOF | sudo tee /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
@@ -14,10 +14,10 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
 deb https://apt.kubernetes.io/ kubernetes-xenial main
 EOF
-sudo apt-cache madison kubeadm |head -n 20 | tr -d " "| awk -F "|" '{print $2}' |tee /tmp/k8s-version-to-install
+sudo apt-cache madison kubeadm |head -n 20 | tr -d " "| awk -F "|" '{print $2}' > /tmp/k8s-version-to-install
 if [ "$VER" == "FIXME" ]
 then
-
+cat  /tmp/k8s-version-to-install
    read -p "Enter the kubernetes version to install the default will $VER  : " K8S
    K8S=${K8S:-"1.20.7-00"}
    if [ "$VER" != "$K8S" ]
