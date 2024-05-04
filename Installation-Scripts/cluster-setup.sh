611f22bbdf7ea07f1ae2dfdde6fe27d91a462aac
DeeKay <kumardineshwar@gmail.com>
2024-05-04 17:37:46 +0200

    Update cluster-setup.sh

--- Installation-Scripts/cluster-setup.sh
+++ Installation-Scripts/cluster-setup.sh
@@ -4,6 +4,7 @@
 rm -f /tmp/setup_containerd.sh /tmp/setup_k8s.sh /tmp/node-join.sh /tmp/metallb-address-pool.yaml
 
 cat<<EOF>> /tmp/setup_containerd.sh
+#!/bin/bash
 
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.30/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
@@ -27,45 +28,50 @@ systemctl restart containerd.service
 
 sudo apt-get install kubeadm kubelet kubernetes-cni -y
 
-kubeadm config images pull
+sleep 10
 
 EOF
 
 
 for i in worker01 worker02 worker03; do scp /tmp/setup_containerd.sh $i:/tmp/setup_containerd.sh; done
 
-
 for i in worker01 worker02 worker03 master01; do ssh $i "sh /tmp/setup_containerd.sh"; done
 
-
 cat<<EOF>> /tmp/setup_k8s.sh
+#!/bin/bash
 
 IP=\$(ip a show ens33 | grep "192.168" | cut -d " " -f6 | cut -d"/" -f1)
 HOST=\$(hostname)
 
+kubeadm config images pull 
+
 apt-get install kubectl -y
+
 kubeadm init --cri-socket /run/containerd/containerd.sock --apiserver-advertise-address \$IP --control-plane-endpoint \$IP --pod-network-cidr 192.168.0.0/16 --node-name \$HOST --v=7
 
 wget https://raw.githubusercontent.com/projectcalico/calico/v3.27.3/manifests/calico.yaml -O /tmp/calico.yaml
 
-sleep 60
+export KUBECONFIG=/etc/kubernetes/admin.conf
 
+kubeadm token create --print-join-command --ttl 0 2>/dev/null | tee /tmp/node-join.sh
 
-mkdir -p /root/.kube/
-cp /etc/kubernetes/admin.conf /root/.kube/config
-kubectl apply -f /tmp/calico.yaml
-
+mkdir -p /root/.kube
+sudo cp -i /etc/kubernetes/admin.conf /root/.kube/config
 echo 'source <(kubectl completion bash)' >> /root/.bashrc
 
-source /root/.bashrc
 
-kubeadm token create --print-join-command --ttl 0 2>/dev/null > /tmp/node-join.sh
+while [ \$(kubectl get nodes --no-headers | grep -c master01) -ne 1 ]; do sleep 10; done
+
+sleep 30
+
+kubectl apply -f /tmp/calico.yaml
 
 EOF
 
-sh /tmp/setup_k8s.sh
+bash /tmp/setup_k8s.sh
 
 sleep 10 
+
 for i in worker01 worker02 worker03; do scp /tmp/node-join.sh $i:/tmp/node-join.sh; done
 
 for i in worker01 worker02 worker03; do ssh $i "sh /tmp/node-join.sh"; done
@@ -76,7 +82,7 @@ for i in worker01 worker02 worker03; do ssh $i "rm -f /tmp/setup_containerd.sh /
 
 cp /tmp/node-join.sh node-join.sh
 
-sleep 60
+sleep 30
 
 kubectl get nodes -o wide
 
