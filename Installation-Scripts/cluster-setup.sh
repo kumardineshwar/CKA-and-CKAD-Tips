d1c6238975d2156931e734c303275ebd73a076ca
DeeKay <kumardineshwar@gmail.com>
2024-05-04 20:58:25 +0200

    Update cluster-setup.sh

--- Installation-Scripts/cluster-setup.sh
+++ Installation-Scripts/cluster-setup.sh
@@ -11,6 +11,7 @@ sudo chmod 644 /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.30/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo chmod 644 /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
+modprobe br_netfilter
 echo "net.ipv4.ip_forward = 1" | sudo tee /etc/sysctl.d/k8s.conf
 sudo sysctl --system
 sysctl net.ipv4.ip_forward
